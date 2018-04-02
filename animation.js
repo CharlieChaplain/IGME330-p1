c8c1db460776d75c70347d1fc082149c9b0c3fa6
Stephen Callen <src3363@rit.edu>
2018-04-01 21:47:33 -0400

    Added better character sprite and made it work. I'm going to add an arrow in front of the player to better show where he's facing

--- animation.js
+++ animation.js
@@ -3,12 +3,14 @@
 var app = app || {};
 
 app.animation = (function(){
-    function Animation(image, numFrames, secondsPerFrame, frameWidth, frameHeight){
+    function Animation(image, numFrames, secondsPerFrame, frameWidth, frameHeight, xCoord, scale){
                 this.image = image;
                 this.numFrames = numFrames;
                 this.spf = secondsPerFrame;
                 this.width = frameWidth;
                 this.height = frameHeight;
+                this.xCoord = xCoord; //how far in the sprite sheet the animation starts
+                this.scale = scale;
                 this.currentFrame = 0;
                 this.animTimer = 0;
     }
@@ -27,7 +29,7 @@ app.animation = (function(){
     }
 
     Animation.prototype.drawAnimation = function(ctx, x, y){
-        ctx.drawImage(this.image, this.width * this.currentFrame, 0, this.width, this.height, x, y, this.width, this.height);
+        ctx.drawImage(this.image, this.width * (this.currentFrame + this.xCoord), 0, this.width, this.height, x, y, this.width*this.scale, this.height*this.scale);
     }
     
     // export a public interface to this module
